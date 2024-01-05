6e112522b2589e2f054906a75fdcee57d555e060
Shreyas Jayaprakash <126165002+shreyasjp@users.noreply.github.com>
2024-01-06 02:11:15 +0530

    hehe

--- Splitting/test.py
+++ Splitting/test.py
@@ -1,12 +1,17 @@
 import mysql.connector as a
-b=a.connect(host='localhost', user='root', passwd='root');
-d=b.cursor();
+b=a.connect(host='localhost', user='root', passwd='root')
+d=b.cursor(buffered=True)
 d.execute('SELECT * FROM test.rooms JOIN test.room_class_specs ON test.rooms.RoomClassID = test.room_class_specs.RoomClassID;')
 
-for i in d.fetchall():
-    print(i)
-
 d.execute('select * from test.prn_sub;')
+e=d.column_names;
+f=list(e)
+f.pop(0)
+print(f)
 
-for i in d.fetchall():
+for i in f:
     print(i)
+    d.execute('select `'+i+'` from test.prn_sub where `'+i+'` is not null;')
+    g=d.fetchall()
+    for j in g:
+        print(j[0])
