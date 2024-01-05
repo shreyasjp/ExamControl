c05d6be2ac61fb4b35e8a33d4b69679c6610180e
Shreyas Jayaprakash <126165002+shreyasjp@users.noreply.github.com>
2024-01-06 03:16:33 +0530

    Update test.py

--- Splitting/test.py
+++ Splitting/test.py
@@ -1,9 +1,24 @@
+class RoomInfo:
+    def __init__(self, room_id, room_name, priority, total_seats, filled_seats, rows, columns, matrix):
+        self.RoomID = room_id
+        self.RoomName = room_name
+        self.Priority = priority
+        self.TotalSeats = total_seats
+        self.FilledSeats = filled_seats
+        self.Rows = rows
+        self.Columns = columns
+        self.Matrix = matrix
+        self.IsFilled = self.calculate_is_filled()
+
+    def calculate_is_filled(self):
+        return self.FilledSeats == self.TotalSeats
+
 import mysql.connector as a
 b=a.connect(host='localhost', user='root', passwd='root')
 d=b.cursor(buffered=True)
 d.execute('SELECT * FROM test.rooms JOIN test.room_class_specs ON test.rooms.RoomClassID = test.room_class_specs.RoomClassID;')
 
-d.execute('select * from test.prn_sub;')
+""" d.execute('select * from test.prn_sub;')
 e=d.column_names;
 f=list(e)
 f.pop(0)
@@ -13,6 +28,16 @@ for i in f:
     print('\n',i,'\n')
     z='select `'+i+'` from test.prn_sub where `'+i+'` is not null;'
     d.execute(z)
+    rows = d.rowcount
+    print(rows,'\n')
     g=d.fetchall()
     for j in g:
-        print(j[0])
+        print(j[0]) """
+
+d.execute('SELECT * FROM test.rooms JOIN test.room_class_specs ON test.rooms.RoomClassID = test.room_class_specs.RoomClassID;')
+rooms=[]
+for i in d.fetchall():
+    matrix=[[0] * i[8] for _ in range(i[7])]
+    rooms.append(RoomInfo(i[0],i[1],i[5],i[6],0,i[8],i[7],matrix))
+
+print(rooms[0].RoomName)
